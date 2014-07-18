46753658ef47f2348c6925c770fe62c797b4213d
Pral2a <guillem@pral2a.com>
2014-07-18 03:29:56 +0200

    Firt Release

---
+++ README.md
@@ -0,0 +1,5 @@
+## URL File Update notifier
+
+Check a file hosted on a URL perdiodically and send a notiication email when it change.
+
+This is just a draft.
---
+++ app.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+var config = require('./config.json');
+var http = require('http');
+var fs = require('fs');
+var crypto = require('crypto');
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport(config.email_notifications_settings.server);
+
+
+var request = http.get(config.download_url, function (response) {
+
+    var hash = crypto.createHash('sha1');
+
+    var storeObject = {};
+
+    response.on('data', function (data) {
+        hash.update(data);
+    });
+
+    response.on('end', function () {
+        fs.readFile(config.store_file, function (err, data) {
+            if (err) return console.log(err);
+
+            storeObject = JSON.parse(data);
+
+            var newStoreObject = {
+                time: new Date().toString(),
+                url: config.download_url,
+                hash: hash.digest('hex')
+            }
+
+            var isUpdated = (storeObject.hash != newStoreObject.hash) ? true : false;
+
+            if (isUpdated) {
+                config.email_notifications_settings.content.subject  += ( " - " + config.email_notifications_settings.content.variableContent.updated);
+                config.email_notifications_settings.content.html = "<h1 style='color: #D60024;'>" + config.email_notifications_settings.content.variableContent.updated + "</h1><code>" +  JSON.stringify(newStoreObject) + "</code>";
+            } else {
+                config.email_notifications_settings.content.subject  += ( " - " + config.email_notifications_settings.content.variableContent.fine);
+                config.email_notifications_settings.content.html = "<h1 style='color: #00D627;'>" + config.email_notifications_settings.content.variableContent.fine + "</h1><code>" +  JSON.stringify(newStoreObject) + "</code>";
+            }
+
+            console.log(config.email_notifications_settings.content);
+
+			transporter.sendMail(config.email_notifications_settings.content, function(error, info){
+			    if(error){
+			        console.log(error);
+			    }else{
+			        console.log('Message sent: ' + info.response);
+			    }
+			});
+
+            fs.writeFile(config.store_file, JSON.stringify(newStoreObject), function (err) {
+                if (err) return console.log(err);
+            });
+
+        });
+    });
+});
